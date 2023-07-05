daa3f3bb09c78238c36bd6d4a71089d44905dbab
Anmol Verma <115978470+Anmol26verma@users.noreply.github.com>
2023-07-05 23:44:07 +0530

    index.html

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
+ // Get the root HTML element
+ const root = document.documentElement;
 
+ // Disable the scrollbar by setting the overflow property to hidden
+ root.style.overflow = 'hidden';
+
+ // Enable the scrollbar by setting the overflow property back to its default value
+ root.style.overflow = 'auto';
